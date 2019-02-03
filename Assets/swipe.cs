b8a0614ae59664121781e6d9d12d83b42a927d41
Oliver Jean <ojean128@gmail.com>
2019-02-02 20:52:10 -0500

    phone test

---
+++ Assets/swipe.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swipe : MonoBehaviour
+{
+
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private bool isDragging = false;
+    private Vector2 startTouch, swipeDelta;
+
+    private void Update()
+    {
+        tap = swipeLeft = swipeUp = swipeRight = swipeDown = false;
+        
+        #region Standalone Inputs
+        if (Input.GetMouseButtonDown(0))
+        {
+            tap = true;
+            isDragging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Inputs
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = true;
+                isDragging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragging = false;
+                Reset(); 
+            }
+        }
+        #endregion
+
+        //Distance
+        swipeDelta = Vector2.zero;
+        if (isDragging)
+        {
+            if (Input.touches.Length > 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+
+
+        //Cross the deadzone:
+        if(swipeDelta.magnitude > 50)
+        {
+            //Direction
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                if (x < 0)
+                {
+                    swipeLeft = true;
+                    Debug.Log("LEFT");
+                }
+                else
+                {
+                    swipeRight = true;
+                    Debug.Log("RIGHT");
+                }
+            }
+            else
+            {
+                if (y < 0)
+                {
+                    swipeDown = true;
+                    Debug.Log("DOWN");
+                }
+                else 
+                { 
+                    swipeUp = true;
+                    Debug.Log("UP");
+                }
+
+            }
+            }
+
+            Reset();
+        }
+
+
+
+
+    // Update is called once per frame
+    private void Reset()
+    {
+        Debug.Log("RESET");
+        startTouch = swipeDelta = Vector2.zero;
+        isDragging = false;
+    }
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeDown { get { return swipeDown; } }
+    public bool Tap { get { return tap; } }
+}
